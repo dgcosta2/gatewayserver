d865c2bc42f9de3bd006f5568449e0bc6b5c9d46
JoshSensanb <111921798+JoshSensanb@users.noreply.github.com>
2023-04-24 17:57:59 -0400

    Gateway class start

--- src/main/java/com/optimagrowth/gatewayserver/ApiGatewayServerApplication.java
+++ src/main/java/com/optimagrowth/gatewayserver/ApiGatewayServerApplication.java
@@ -2,6 +2,7 @@ package com.optimagrowth.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEureka;
 
 @SpringBootApplication
 public class ApiGatewayServerApplication {
